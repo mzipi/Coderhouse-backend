252e854e3acfe2fb1741335f8f886ba746d7ad7b
Michell Zipitría <mzipi@outlook.com>
2022-09-06 02:57:14 -0300

    plantillas mejoradas

--- 6 - Websockets/routes/api_router.js
+++ 6 - Websockets/routes/api_router.js
@@ -5,41 +5,41 @@ const data = new ContenedorMemoria();
 
 router.get('/', (req, res) => {
     if(data.getAll().length > 0) {
-        res.render('products', { mensaje: data.getAll(), titulo: 'Todos los productos' })
+        res.render('products', { mensaje: data.getAll(), title: 'Todos los productos' })
     } else {
-        res.render('no-products', { titulo: 'Todos los productos' });
+        res.render('no-products', { title: 'Todos los productos' });
     }
 });
 
 router.get('/:id', ({ params }, res) => {
     if(data.getAll().length > 0) {
         let obj = data.getOne(params.id)
-        res.render('products', { mensaje: obj, titulo: 'Vista del producto'});
+        res.render('products', { mensaje: obj, title: 'Vista del producto'});
     } else {
-        res.render('no-products', { titulo: 'Vista del producto' });
+        res.render('no-products', { title: 'Vista del producto' });
     }
 });
 
 router.post('/', ({ body }, res) => {
     let obj = data.addOne(body)
-    res.render('products', { mensaje: [obj], titulo: "Producto agregado" })
+    res.render('products', { mensaje: [obj], title: "Producto agregado" })
 });
 
 router.put('/:id', ({ params, body}, res) => {
     if(data.getAll().length > 0) {
         let arr = data.updateOne(params.id, body);
-        res.render('products', { mensaje: arr, titulo: "Producto actualizado"})
+        res.render('products', { mensaje: arr, title: "Producto actualizado"})
     } else {
-        res.render('no-products', { titulo: "Producto actualizado" });
+        res.render('no-products', { title: "Producto actualizado" });
     }
 });
 
 router.delete('/:id', ({ params }, res) => {
     if(data.getAll().length > 0) {
         let obj = data.deleteOne(params.id);
-        res.render('products', { mensaje: obj, titulo: 'Producto eliminado' })
+        res.render('products', { mensaje: obj, title: 'Producto eliminado' })
     } else {
-        res.render('no-products', { titulo: 'Producto eliminado' });
+        res.render('no-products', { title: 'Producto eliminado' });
     }
 });
 
--- 6 - Websockets/routes/main-router.js
+++ 6 - Websockets/routes/main-router.js
@@ -4,6 +4,6 @@ const ContenedorMemoria = require('../contenedor');
 const data = new ContenedorMemoria();
 
 // router.get('/', (req, res) => res.render('form'));
-router.get('/', (req, res) => res.render('form', { mensaje: data.getAll() }));
+router.get('/', (req, res) => res.render('index', { title: "Lista de productos"}));
 
 module.exports = router;
