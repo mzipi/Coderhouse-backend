b188ffa19229c31970e9cdda3ec392d2afa9dea1
Michell Zipitría <mzipi@outlook.com>
2022-10-12 22:07:18 -0300

    initial commit

--- 12 - Login por formulario/server.js
+++ 12 - Login por formulario/server.js
@@ -1,6 +1,22 @@
 import { express } from "express";
-import { cookieParser } from "cookie-parser";
+import { session } from "express-session";
 
 const app = express();
 
-app.use(cookieParser());
+var PORT = process.env.port || 8080
+
+app.use(session({
+    secret: 'secreto',
+    resave: true,
+    saveUninitialize: true
+}));
+
+app.get('/login', (req, res) => {
+    const { username, password } = req.query;
+    if (username !== 'pepe' || password !== 'pepepass') {
+        return res.send('login failed');
+    };
+    req.session.user = username;
+    req.session.admin = true;
+    res.send('login success!');
+})
