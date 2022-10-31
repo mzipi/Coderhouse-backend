35c17a4f589ec95179179b963df6ce0ff0a94ae0
Michell Zipitría <mzipi@outlook.com>
2022-10-31 06:35:17 -0300

    child process

--- 14 - Usando el objeto process/router/api_randoms-router.js
+++ 14 - Usando el objeto process/router/api_randoms-router.js
@@ -1,6 +1,16 @@
 const { Router } = require("express");
+const { fork } = require("child_process");
 
 const router = Router();
+
+const computo = fork('./computo.js')
+computo.on('message', msg => {
+    if (msg === 'listo') {
+        computo.send('calcular!')
+    } else {
+        res.end(`La suma es ${msg}`)
+    }
+})
 
 router.get("/", (req, res) => {
     res.render("api-randoms");
--- 14 - Usando el objeto process/router/info-router.js
+++ 14 - Usando el objeto process/router/info-router.js
@@ -9,8 +9,6 @@ const mem = process.memoryUsage().rss;
 const argv = process.argv.slice(2);
 const path = process.argv[0]
 
-console.log(process.argv[0]);
-
 router.get("/", (req, res) => {
     res.render("info", { cwd, pid, ver, platform, mem, argv, path });
 });
