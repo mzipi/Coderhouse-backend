2c2ea1e0f5e6128110a634a143f1819eba3f091e
Michell Zipitría <mzipi@outlook.com>
2022-11-13 08:12:08 -0300

    tarea terminada

---
+++ 17 - Desplegar nuestro proyecto en la nube/router/login-router.js
@@ -0,0 +1,18 @@
+const { Router } = require("express");
+const passport  = require("passport");
+
+const router = Router();
+
+router.get("/", (req, res) => {
+    res.render("login");
+});
+
+router.post(
+    "/", 
+    passport.authenticate("login", { failureRedirect: "/login" }), 
+    (req, res) => {
+        res.redirect("/");
+    }
+);
+
+module.exports = router;
