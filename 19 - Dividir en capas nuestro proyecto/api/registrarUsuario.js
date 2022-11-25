d4aedc2e2636820243c82314e0bd747cb864c8c9
Michell Zipitría <mzipi@outlook.com>
2022-11-25 17:59:46 -0300

    iniciando tarea

---
+++ 19 - Dividir en capas nuestro proyecto/api/registrarUsuario.js
@@ -0,0 +1,23 @@
+function registrarUsuario(datosUsuario) {
+
+    function crearId() {
+        return `${Date.now()}`
+    }
+    function crearUsuario({ id = crearId(), username, password, direccion }) {
+        if (!username) throw new Error(`MISSING_ARGS: el campo "username" es obligatorio`)
+        if (!password) throw new Error(`MISSING_ARGS: el campo "password" es obligatorio`)
+        if (!direccion) throw new Error(`MISSING_ARGS: el campo "direccion" es obligatorio`)
+
+        return {
+            id,
+            username,
+            password,
+            direccion,
+        }
+    }
+    const usuario = crearUsuario(datosUsuario)
+    guardarUsuario(usuario)
+    return usuario
+}
+
+module.exports = registrarUsuario;
