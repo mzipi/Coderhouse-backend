ac3a6d6a9fd71bba3c104343c9b0c8557e6b3914
Michell Zipitría <mzipi@outlook.com>
2022-11-25 20:23:53 -0300

    organizando repo

--- 5 - Motores de plantillas/pug/routes/main-router.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => res.render('form.pug', { titulo: "Agregar producto" }))
-
-module.exports = router;
