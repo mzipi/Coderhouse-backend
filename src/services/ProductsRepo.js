dbf88600d6a649cfc451914a873deab20c4e687e
Michell Zipitría <mzipi@outlook.com>
2022-12-04 00:11:12 -0300

    tarea terminada

--- src/services/ProductsRepo.js
+++ src/services/ProductsRepo.js
@@ -5,23 +5,20 @@ export default class ProductsRepo {
         this.dao = container;
     }
 
-    // espera recibir una instancia de la clase Usuario
     async setData(data) {
         await this.dao.setData(data.asDto())
     }
 
-    // devuelve un array de instancias de Usuario
     async getAllData() {
         const dtos = await this.dao.getAllData()
         return dtos.map(dto => new Product(dto))
     }
 
-    async updateData(data) {
-        await this.dao.updateData(data.asDto())
-    }
+    // async updateData(data) {
+    //     await this.dao.updateData(data.asDto())
+    // }
 
-    // devuelve un array de instancias de Usuario
-    async delData(data) {
-        await this.dao.delData(data.asDto())
-    }
+    // async delData(data) {
+    //     await this.dao.delData(data.asDto())
+    // }
 }
