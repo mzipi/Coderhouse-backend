9a9bbf7b2981c9b3398163a831a79fd96d190496
Michell Zipitría <mzipi@outlook.com>
2023-01-16 18:59:13 -0300

    tarea terminada

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
-import { serve } from "https://deno.land/std@0.157.0/http/server.ts";
+import { serveListener } from "https://deno.land/std@0.157.0/http/server.ts";
 
-serve(() => new Response("Hello world!"));
+const listener = Deno.listen({ port: 8000 });
+
+await serveListener(listener, (request) => {
+  let body = "";
+
+  if(request.url.endsWith('/') && request.method === 'GET') {
+    body = "GET /"
+  }
+
+  return new Response(body, { status: 200 });
+});
