9a29c127b534fd4c6c060efbbe2328f99270084e
Michell Zipitría <mzipi@outlook.com>
2023-02-10 16:03:21 -0300

    Update mongodb.js

--- options/mongodb.js
+++ options/mongodb.js
@@ -1,10 +1,12 @@
+const { MONGO_USR, MONGO_PWD, MONGO_DB } = require("../config.js");
+
 const options = {
     client: 'mongodb',
     connection: {
         host: 'localhost',
-        user: 'root',
-        password: 'mongodbpassword',
-        database: 'ecommerce'
+        user: MONGO_USR,
+        password: MONGO_PWD,
+        database: MONGO_DB
     }
 }
 export default options
