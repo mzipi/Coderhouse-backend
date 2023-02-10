137e7be26335a6cb982cc1f85874dbda3a1281de
Michell Zipitría <mzipi@outlook.com>
2023-02-10 16:08:22 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,9 @@ import { config } from "dotenv";
 
 config();
 
+const MARIADB_USR = process.env.MARIADB_USR;
+const MARIADB_PWD = process.env.MARIADB_PWD;
+const MARIADB_DB = process.env.MARIADB_DB;
 const MONGO_URL = process.env.MONGO_URL;
 const MONGO_USR = process.env.MONGO_USR;
 const MONGO_PWD = process.env.MONGO_PWD;
@@ -11,6 +14,9 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const CLIENT_EMAIL = process.env.CLIENT_EMAIL;
 
 export {
+    MARIADB_USR,
+    MARIADB_PWD,
+    MARIADB_DB,
     MONGO_URL,
     MONGO_USR,
     MONGO_PWD,
